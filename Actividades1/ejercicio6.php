f297db19ea36ffc46e077e8d1490807dd2a0c101
Veselin Vladimirov <veselin86@gmail.com>
2023-10-18 00:03:24 +0200

    Add ejercicio6.php

---
+++ Actividades1/ejercicio6.php
@@ -0,0 +1,38 @@
+<?php
+
+    class Mamifero {
+        protected $especie;
+        protected $sonido;
+        protected $familia;
+
+        public function __construct($especie, $sonido) {
+            $this -> especie = $especie;
+            $this -> sonido = $sonido;
+        }
+
+        public function sonido(){
+            return "Sonido de " . $this -> especie . ", de la familia " . $this -> familia . ": " . $this -> sonido;
+        }
+    }
+
+    class Perro extends Mamifero{
+        public function __construct(string $familia) {
+            parent :: __construct("perro", "bau bau");
+            $this -> familia = $familia;
+        }
+    }
+
+    class Gato extends Mamifero{
+        public function __construct(string $familia) {
+            parent :: __construct("gato", "miau miau");
+            $this -> familia = $familia;
+        }
+    }
+
+    $perro = new Perro("canidos");
+    $gato = new Gato("felinos");
+
+    echo $perro -> sonido() . "<br>";
+    echo $gato -> sonido() . "<br>";
+
+?>
